d565617e4d1b4239c124415984869383ad2a7dd5
Azraf Chowdhury <achowdhur@ltu.edu>
2019-01-21 07:59:42 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,26 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true 
+
+     table_col = DB[:conn].execute("PRAGMA table_info(#{self.table_name})")
+    col_names = []
+
+     table_col.each do |col|
+      col_names << col["name"]
+    end
+    col_names.compact
+ end
 end
